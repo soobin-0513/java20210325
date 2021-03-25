304cc71b181144b1d1134abf273067af98405cbc
soobin-0513 <rxb0513@gmail.com>
2021-03-25 11:32:58 +0900

    chapter 01

---
+++ src/P01/textbook/RunStatementExample.java
@@ -0,0 +1,10 @@
+package P01.textbook;
+
+public class RunStatementExample {
+	 static void main(String[]args) {
+		int x =1; //변수x를 선언하고 1을 저장
+		int y =2; //변수y를 선언하고 2을 저장
+		int result = x+y; //변수 result를 선언하고 x와y를 더한값을저장
+		System.out.println(x+"+"+y+"="+result);//콘솔에 출력하는 메소드 호출
+	}
+}
