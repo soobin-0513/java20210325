18e3e23ff471323ca1d89add2f5f9d9d81dd1013
soobin-0513 <rxb0513@gmail.com>
2021-03-25 17:55:41 +0900

    int

--- src/P02/textbook/VariableScopeExample.java
+++ src/P02/textbook/VariableScopeExample.java
@@ -6,6 +6,6 @@ public class VariableScopeExample {
 		if(v1>10) {
 			int v2 = v1 -10;
 		}
-		int v3 = v1 +v2 +5; //v2변수를 사용할수없기때문에 컴파일 에러가 생김
+		int v3 = v1 + v2 +5; //v2변수를 사용할수없기때문에 컴파일 에러가 생김
 	}
 }
