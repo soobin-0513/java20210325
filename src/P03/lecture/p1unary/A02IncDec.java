6e9d132639d312f3923e1aa44cb0b422ea652c62
soobin-0513 <rxb0513@gmail.com>
2021-03-26 16:26:57 +0900

    commit

--- src/P03/lecture/p1unary/A02IncDec.java
+++ src/P03/lecture/p1unary/A02IncDec.java
@@ -13,5 +13,22 @@ public class A02IncDec {
 		// -- : 변수에 1뺀 값을 다시 변수에 할당
 		x--;
 		System.out.println(x);
+		
+		/////////////////
+		//연산자가 앞에 있을때
+		//++x; //밑에처럼 작성하는게 좋음
+		x= x + 1;;
+		System.out.println(x);
+		
+		int y = 5 +(++x);
+		System.out.println(y);
+		
+		////////////
+		//연산자가 뒤에있을때 , 다른연산을 한이후에 연산이됨.
+		System.out.println(x);
+		y= 5 + (x ++);  //가능한 이렇게 복잡하게 작성하면 안됨
+		System.out.println(y);
+		
+		
 	}
 }
