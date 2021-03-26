67aef3ef3ce5b11fc19ac16405b1b410b2c03a3e
soobin-0513 <rxb0513@gmail.com>
2021-03-26 15:37:07 +0900

    sign

--- src/P03/lecture/A01Sign.java
+++ src/P03/lecture/A01Sign.java
@@ -1,7 +1,18 @@
-package P03.lecture;
+package p03.lecture.p1unary;
 
 public class A01Sign {
 	public static void main(String[] args) {
+		int x = 100;
+		int y = -100;
+		int z = -x;
+		
+		int a = -y;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(a);
+		
 		
 	}
 }
---
+++ src/P03/text/SignOpearatorExample.java
@@ -0,0 +1,11 @@
+package P03.text;
+
+public class SignOpearatorExample {
+	public static void main(String[] args) {
+		 int x = -100;
+		 int result1 = +x;
+		 int result2 = -x;
+		 System.out.println("result1="+result1);
+		 System.out.println("result2="+result2);
+	}
+}
