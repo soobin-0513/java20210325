5c54b712fa0f18b117b7858694091828710f6d50
soobin-0513 <rxb0513@gmail.com>
2021-03-30 14:07:49 +0900

    if

---
+++ src/p04/lecture/A01If.java
@@ -0,0 +1,5 @@
+package p04.lecture;
+
+public class A01If {
+	//if " 조건이 true 일때만
+}
---
+++ src/p04/textbook/IfExample.java
@@ -0,0 +1,15 @@
+package p04.textbook;
+
+public class IfExample {
+	public static void main(String[] args) {
+		 int score = 93;
+		 if(score>=90) {
+			 System.out.println("점수가 90보다큽니다");
+			 System.out.println("등급이 A입니다");
+		 }
+		 if(score<90) {
+			 System.out.println("점수가 90보다 적습니다");
+			 System.out.println("등급은 B입니다.");
+		 }
+	}
+}
