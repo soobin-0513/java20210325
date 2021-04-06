a3faffe72d8fb5fd38177a8af6a7f110a1f485a1
soobin-0513 <rxb0513@gmail.com>
2021-04-06 12:16:02 +0900

    class

---
+++ src/p06/lecture/p1class/A01Class.java
@@ -0,0 +1,14 @@
+package p06.lecture.p1class;
+
+public class A01Class {
+	public static void main(String[] args) {
+		//javascript에세 객체(object)만들기
+		/*
+		  var obj ={name:"john",age:30,city:"seoul"};
+		 */
+		//java에서 객체(object,instance) 만들기
+		//객체를 만들기 위한 설계도(class) 꼭 필요*****
+		String str = new String("java");
+		Student student = new Student();
+	}
+}
