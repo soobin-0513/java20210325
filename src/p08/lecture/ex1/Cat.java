f8fe234540b8e9d00c9ce288c44b614afd8e7c8e
soobin-0513 <rxb0513@gmail.com>
2021-04-14 09:33:50 +0900

    interface

---
+++ src/p08/lecture/ex1/Cat.java
@@ -0,0 +1,11 @@
+package p08.lecture.ex1;
+
+public class Cat extends KindaCat implements Pet{
+
+	@Override
+	public void sit() {
+		System.out.println("고양이기 앉아있습니다");
+		
+	}
+
+}
---
+++ src/p08/lecture/ex1/Pet.java
@@ -0,0 +1,6 @@
+package p08.lecture.ex1;
+
+public interface Pet {
+	//interface은 추상적인거 abstract 생략되있음 ,public생략해도 public
+	void sit();  //public abstract void  sit();
+}
