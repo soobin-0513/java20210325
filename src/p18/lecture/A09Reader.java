e071ba48d6035a48f4aa17e97a9b816a06a361c8
soobin-0513 <rxb0513@gmail.com>
2021-05-03 12:03:04 +0900

    Reader

---
+++ src/p18/lecture/A09Reader.java
@@ -0,0 +1,34 @@
+//한글
+package p18.lecture;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.Reader;
+
+public class A09Reader {
+
+	public static void main(String[] args) throws Exception {
+		
+		//byte 단위
+		String file ="src/p18/lecture/A09Reader.java";
+		InputStream is =new FileInputStream(file);
+		System.out.println((char) is.read());
+		System.out.println((char) is.read());
+		System.out.println((char) is.read());
+		System.out.println((char) is.read());
+		
+		
+		is.close();
+		
+		//문자단위
+		Reader rd = new FileReader(file);
+		System.out.println((char) rd.read());
+		System.out.println((char) rd.read());
+		System.out.println((char) rd.read());
+		System.out.println((char) rd.read());
+		rd.close();
+
+	}
+
+}
